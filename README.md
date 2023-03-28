072e1074cf7359457c5fdca18619d7c9d342ae09
Amit Pal <amitpal16181@gmail.com>
2023-03-28 23:54:53 +0530

    Chnages with API Keys removed from public code

---
+++ README.md
@@ -0,0 +1,17 @@
+# Running the application in local machine
+### Node should be installed already in your machine to run this application. If it is not installed, you can do that from [Node.JS Website](https://nodejs.org/en/download)
+### You can learn more about the working of Node.JS from their [documentation](https://nodejs.org/en/docs)
+
+To run this application, clone or download all the files in your local machine and then install node modules using following commands:<br>
+`npm install express`<br>
+`npm install body-parser`<br>
+`npm install request`
+
+You can also install all the three modules in a single command using the command:<br>
+`npm install express body-parser request`
+
+After successfullly intalling the modules, use one of the following command to run the server:<br>
+`node app.js`
+`nodemon app.js`
+
+You can google the difference between both commands and learn about them.
---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const https = require('https');
+const request = require('request');
+
+const app = express();
+
+app.use(express.static("public"));              // set a folder as static and make all static files public making it accessible to the client browser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    // console.log("Server is up and running...");
+    res.sendFile(__dirname + "/signup.html");;
+});
+
+app.post("/", function (req, res) {
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+    console.log(firstName, lastName, email);
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us13.api.mailchimp.com/3.0//lists/{YOUR_UNIQUE_AUDIENCE_ID}";
+    const options = {
+        method: "POST",
+        auth: "{ANY_TEXT}:{YOUR_API_KEY}"               // Read node documentation for http to know working and more.
+    };
+
+
+    const httpsRequest = https.request(url, options, function (response) {
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        });
+    });
+
+    httpsRequest.write(jsonData);
+    httpsRequest.end();
+
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Server is up and running on the port " + port);
+});
+
+// Mailchimp API :-  e863dc01eb3a63d5854aba600391df2a-us13
+// Unique Audience ID :- 2adcfbef52
